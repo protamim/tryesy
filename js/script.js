916e7ff599d3351c337c1f418079eef80e165cd4
Tamim Talukdar <s163.tamim@gmail.com>
2024-03-20 09:36:03 +0600

    Navbar with responsive

---
+++ js/script.js
@@ -0,0 +1,6 @@
+// console.log('connected js in HTML');
+$(document).ready(() => {
+  $("#menu-bar").click(() => {
+    $("#mobile-nav").toggleClass("!left-0");
+  });
+});
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary-color': '#A4FF5C',
+        'secondary-color': '#FFEF5B',
+        'body-color': '#141820'
+      }
+    },
+  },
+  plugins: [],
+}
+
